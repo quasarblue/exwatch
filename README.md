9e24d8c4a90759fd7c29e4323ae1e73bdd02db71
quasarblue <quazarblue@gmail.com>
2022-02-21 09:37:12 +0100

    vacuum

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # exwatch
+
+setup database script is in /db directory
+
 to compile:
 mvn clean package
 
 to run:
 mvn spring-boot:run
+
+
+add to crontab: get_cmc_data.sh and dbupdate.sh
+for updating coinmarketcap data (need coinmarketcap api key)
 
--- x.sh
+++ x.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 read -r CMC_API_KEY
-curl -s -H "X-CMC_PRO_API_KEY: $CMC_API_KEY" -H "Accept: application/json" -d "" -G "https://pro-api.coinmarketcap.com/v1/exchange/map" | jq '.data[] | .id, .name' | awk '{id=$0; getline; name=$0; print id ";" name}'| sed 's/"//g' | awk 'BEGIN{FS=";"}{print $1}'> loop
+curl -s -H "X-CMC_PRO_API_KEY: $CMC_API_KEY" -H "Accept: application/json" -d "" -G "https://pro-api.coinmarketcap.com/v1/exchange/map" | jq '.data[] | .id, .name' | awk '{id=$0; getline; name=$0; print id ";" name}'| sed 's/"//g' | awk 'BEGIN{FS=";"}{print $1}'> $HOME/git/exwatch/data/cmc_api_exchanges_id.csv 
 while read id
 do
   curl -s -H "X-CMC_PRO_API_KEY: $CMC_API_KEY" -H "Accept: application/json" -d "id=$id" -G "https://pro-api.coinmarketcap.com/v1/exchange/info" | jq '.data[] | .id, .name, .weekly_visits, .spot_volume_usd'|awk '{id=$0; getline; name=$0; getline; wv=$0; getline; sv=$0; print id ";" name ";" wv ";" sv}'| sed 's/"//g'
   sleep 2
-done < "${1:-loop}"
+done < "${1:-$HOME/git/exwatch/data/cmc_api_exchanges_id.csv}"
 
 #xargs -I{} curl -s -H "X-CMC_PRO_API_KEY: $CMC_API_KEY" -H "Accept: application/json" -d 'id={}' -G "https://pro-api.coinmarketcap.com/v1/exchange/info"
