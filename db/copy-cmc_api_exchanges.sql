3bc1a3cafa2bb5eeafbc39d94e822ecee53062f0
quasarblue <quazarblue@gmail.com>
2022-03-03 11:12:53 +0100

    crontab paths

--- db/copy-cmc_api_exchanges.sql
+++ db/copy-cmc_api_exchanges.sql
@@ -1,2 +1,2 @@
 delete from cmc_api_exchange;
-\copy cmc_api_exchange(id, name, weekly_visits, spot_volume_usd) from './data/cmc_api_exchanges.csv' delimiter ';' null as 'null' csv;
+\copy cmc_api_exchange(id, name, weekly_visits, spot_volume_usd) from '~/git/exwatch/data/cmc_api_exchanges.csv' delimiter ';' null as 'null' csv;
--- dbupdate.sh
+++ dbupdate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-psql -d exwatch -U nem -f $HOME/git/exwatch/db/copy-cmc_api_exchanges.sql
-psql -d exwatch -U nem -f $HOME/git/exwatch/db/update-cmc_api_exchange_data.sql
+psql -d exwatch -U nem -W --no-password -f $HOME/git/exwatch/db/copy-cmc_api_exchanges.sql
+psql -d exwatch -U nem -W --no-password -f $HOME/git/exwatch/db/update-cmc_api_exchange_data.sql
